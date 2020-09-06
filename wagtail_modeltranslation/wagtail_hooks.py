c842b97f62fb06eb48523cebf53db428b35ad723
Donald Buczek <buczek@molgen.mpg.de>
2020-09-06 19:46:15 +0200

    Add WAGTAILMODELTRANSLATION_LOCALE_PICKER_DEFAULT
    Make the set of languages, which are initially enabled on the edit pages
    when WAGTAILMODELTRANSLATION_LOCALE_PICKER is used, configurable:
    
        WAGTAILMODELTRANSLATION_LOCALE_PICKER_DEFAULT = None
    
    This is the default, only the default languge will initially be enabled.
    
        WAGTAILMODELTRANSLATION_LOCALE_PICKER_DEFAULT = []
    
    All languages are initially disabled.
    
        WAGTAILMODELTRANSLATION_LOCALE_PICKER_DEFAULT = ['en', 'de']
    
    The given set of languages are enabled.

--- wagtail_modeltranslation/wagtail_hooks.py
+++ wagtail_modeltranslation/wagtail_hooks.py
@@ -14,6 +14,7 @@ from django.views.decorators.csrf import csrf_exempt
 from six import iteritems
 
 from modeltranslation.utils import build_localized_fieldname
+from modeltranslation import settings as mt_settings
 from wagtail_modeltranslation import settings as wmt_settings
 
 from .patch_wagtailadmin_forms import PatchedCopyForm
@@ -48,6 +49,11 @@ def translated_slugs():
     for lang in settings.LANGUAGES:
         lang_codes.append("'%s'" % lang[0])
 
+    if wmt_settings.LOCALE_PICKER_DEFAULT is not None:
+        locale_picker_default = ", ".join(f"'{v}'" for v in wmt_settings.LOCALE_PICKER_DEFAULT)
+    else:
+        locale_picker_default = f"'{mt_settings.DEFAULT_LANGUAGE}'"
+
     js_languages = """
     <script>
         wagtailModelTranslations = {{
@@ -55,13 +61,15 @@ def translated_slugs():
             defaultLanguage: '{language_code}',
             viewEditString: '{view_edit_string}',
             translate_slugs: {translate_slugs},
+            locale_picker_default: [{locale_picker_default}],
         }};
     </script>
     """.format(
         languages=", ".join(lang_codes),
         language_code=settings.LANGUAGE_CODE,
         view_edit_string=_('View / edit fields for'),
-        translate_slugs='true' if wmt_settings.TRANSLATE_SLUGS else 'false'
+        translate_slugs='true' if wmt_settings.TRANSLATE_SLUGS else 'false',
+        locale_picker_default=locale_picker_default
     )
 
     return js_languages + js_includes
